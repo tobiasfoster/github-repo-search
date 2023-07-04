5eb7868b2a88495e9490bc5ac874e7ed4830c908
Tobias Foster <fostertobias@gmail.com>
2023-07-05 00:24:09 +0100

    add general and reusable stylings to index.css

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./config/router.config.ts";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
