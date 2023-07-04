7b2100bd6e7fcccc3ebf20db0cb9c25314953743
Tobias Foster <fostertobias@gmail.com>
2023-07-05 00:18:00 +0100

    update front-end api hook and config

--- web/src/hooks/useGithubApi.ts
+++ web/src/hooks/useGithubApi.ts
@@ -4,6 +4,9 @@ import { apiRoutes } from "../config/api.config";
 
 function useGithubApi() {
   const [getReposResponse, setGetReposResponse] = useState<RepoResponse[]>([]);
+  const [getRepoResponse, setGetRepoResponse] = useState<
+    RepoResponse & { readme: string }
+  >();
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState(false);
 
@@ -16,6 +19,7 @@ function useGithubApi() {
     try {
       setLoading(true);
       setError(null);
+      console.log(apiRoutes.GET_REPOS);
       const response = await fetch(
         `${apiRoutes.GET_REPOS}?query=${query}&sortOrder=${sortOrder}&sortBy=${sortBy}&perPage=${perPage}`,
       );
@@ -25,13 +29,41 @@ function useGithubApi() {
       const error = new Error(
         "An error occurred retrieving Repositories from GitHub",
       );
+      console.log(err);
       setError(error);
     } finally {
       setLoading(false);
     }
   }
 
-  return { getRepos, getReposResponse, error, loading };
+  async function getRepo(owner: string, repo: string) {
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await fetch(
+        `${apiRoutes.GET_REPO}?owner=${owner}&repo=${repo}`,
+      );
+      const data = (await response.json()) as RepoResponse & { readme: string };
+      setGetRepoResponse(data);
+    } catch (err) {
+      const error = new Error(
+        "An error occurred retrieving Repositories from GitHub",
+      );
+      console.log(err);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return {
+    getRepos,
+    getRepo,
+    getReposResponse,
+    getRepoResponse,
+    error,
+    loading,
+  };
 }
 
 export default useGithubApi;
