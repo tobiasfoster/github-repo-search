afec59e663c130f60a4b0200cd6090e3a7565ccf
Tobias Foster <fostertobias@gmail.com>
2023-07-04 21:02:18 +0100

    add model data for sorting controls

---
+++ web/src/models/selectOptions.ts
@@ -0,0 +1,25 @@
+export const SORT_BY_OPTIONS = [
+  {
+    display: "Stars",
+    value: "stars",
+  },
+  {
+    display: "Forks",
+    value: "forks",
+  },
+  {
+    display: "Updated",
+    value: "updated",
+  },
+];
+
+export const SORT_ORDER_OPTIONS = [
+  {
+    display: "Ascending",
+    value: "asc",
+  },
+  {
+    display: "Descending",
+    value: "desc",
+  },
+];
